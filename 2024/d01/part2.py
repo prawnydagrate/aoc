6d370fc7dcf4d84035a0279d5a3e0ffadafb68d0
Pranav <pranav.pooruli@gmail.com>
2024-12-01 14:37:23 +0530

    2024 d1p2 slight optimization

--- 2024/d01/part2.py
+++ 2024/d01/part2.py
@@ -5,9 +5,6 @@ with open('input.txt') as inputf:
     for line in inputf:
         tuple(l.append(n) for l, n in zip((ll, lr), (int(n.strip()) for n in line.split('   '))))
 
-score = 0
-
-for n in ll:
-    score += n * lr.count(n)
+score = sum(n * lr.count(n) for n in ll)
 
 print(score)
