08f2326307c07d8487c2efd99eb679cb7bc78c19
Pranav <pranav.pooruli@gmail.com>
2024-12-03 17:51:17 +0530

    part 1 done, that was underwhelming

--- 2024/d03/getinput.py
+++ 2024/d03/getinput.py
@@ -1,17 +1,17 @@
-INPUT_FILE = None  # comment this line to use input
-TEST_INPUT_INDEX = 0  # change this index to select example
-
 TEST_INPUTS = [
     "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))",
 ]
 
 INPUT_FILE = "input.txt"
 
+# INPUT_FILE = None  # comment this line to use input
+TEST_INPUT_INDEX = 0  # change this index to select example
 
 def getlines():
     if INPUT_FILE is None:
         for line in TEST_INPUTS[TEST_INPUT_INDEX].splitlines():
             yield line
+        return
     with open(INPUT_FILE) as f:
         for line in f:
             yield line
--- 2024/d03/part1.py
+++ 2024/d03/part1.py
@@ -1,3 +1,13 @@
+import math
+import re
+
 from getinput import getall
 
+regex = re.compile(r"mul\(\d+,\d+\)")
+
 s = getall()
+muls = regex.findall(s)
+
+tot = sum(math.prod(int(n) for n in mul[4:-1].split(",")) for mul in muls)
+
+print(tot)
